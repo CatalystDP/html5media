495a603a31af63e996644fcaf4379572798747b8
CatalystDP <cld18874@126.com>
2014-10-18 19:48:33 +0800

    修改serverUploader.js

--- routes/lib/serverUploader.js
+++ routes/lib/serverUploader.js
@@ -1,28 +1,27 @@
 var Request=require('./customrequest');
 var fs=require('fs');
 var path=require('path');
+var util =require('util');
 /**
  * @description 基于http协议进行跨服务器传送文件
  * @param req 来自客户端的请求
  * @param {Object} options 选项
  * @param {String} options.url 要请求的url
- * @param {Object} options.headers http请求头
- * @param {Function} [options.onData(data)] 当有数据时触发
- * @param {Function} [options.onEnd(fileList)] 上传结束时触发
- * @param {Function} [options.onAborted()] 上传取消时触发
+ * @param {String} options.dest 本地上传文件的路径
+ * @param {String} options.filed 表单的域
  */
 exports.uploadToServer=function(req,options){
     var request=new Request();
     var files=req.files[options.filed];
+    var _files=util.isArray(files)? files:[files];
     var r = request.request.post(options.url,function(err,response,body){
-       console.log(body);
         var dest=options.dest;
-        files.forEach(function(item,index){
+        _files.forEach(function(item,index){
            fs.unlinkSync(path.join(dest,item.name));
         });
     });
     var form= r.form();
-    files.forEach(function(item,index){
+    _files.forEach(function(item,index){
         form.append(options.filed,
             fs.createReadStream(path.join(options.dest,item.name)),
             {
