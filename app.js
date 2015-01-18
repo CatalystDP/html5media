1f996b9a6c2c7852eef9b0ad1f950ea835997c17
CatalystDP <cld18874@126.com>
2015-01-18 18:07:07 +0800

    Squashed 'framework/' changes from c054ce1..91e831c
    91e831c 使用stylus作为css预处理器
    
    git-subtree-dir: framework
    git-subtree-split: 91e831cf15bd6e24bba172fd1720a1ff8fce75d2

--- app.js
+++ app.js
@@ -42,7 +42,6 @@ app.use(globalVar);
 app.use('/', routes);
 app.use('/users', users);
 
-
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
