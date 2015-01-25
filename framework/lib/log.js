ac2a248f4ccef644f1c80835c10651c4a3e0d055
CatalystDP <cld18874@126.com>
2015-01-25 10:43:24 +0800

    delete framework

--- framework/lib/log.js
+++
@@ -1,5 +0,0 @@
-var log4js=require('log4js');
-log4js.configure('./config/log_config.json');
-exports.logger=function(name){
-  return log4js.getLogger(name);
-};
--- framework/public/javascripts/app/test.js
+++
@@ -1,5 +0,0 @@
-define(function (require, exports, module) {
-    var $ = require('jQuery');
-    require('../selfmodule/banner.jquery.fix');
-    $('.banner').bannerFade();
-});
